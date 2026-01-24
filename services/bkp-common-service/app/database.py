6d4376dd0edd7b828bfec514b5dd62e22ff8959b
Munna-odcl <munna.odcl@gmail.com>
2026-01-24 17:22:48 +0600

    RootPulse Updt 111

---
+++ services/bkp-common-service/app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load env variables
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()
+
+# Dependency
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
